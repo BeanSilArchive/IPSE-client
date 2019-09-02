45b8054fe575c3770784b60b194e1a4537c85c06
Dotsung <dotsung22@gmail.com>
2019-09-02 11:59:04 +0900

    [UPDATE] Directory Setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
-}
+import { Home } from "pages";
+
+const history = createBrowserHistory();
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router history={history}>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
