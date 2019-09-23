5f08f8aca356d7a64e81be16353a7e30b45d47fc
GwonHeeJun <gwonhj9751@naver.com>
2019-09-23 13:16:14 +0900

    [ADD] CREATE ADDRESS SEARCH

--- src/Modal/Address.js
+++ src/Modal/Address.js
@@ -1,8 +1,30 @@
-import React, { useReducer, useContext } from "react";
+import React, { useReducer, useState } from "react";
+import DaumPostcode from 'react-daum-postcode';
 
 const Address = () => {
-    return (
+  
+    const handleAddress = (data) => {
+        let fullAddress = data.address;
+        let extraAddress = ''; 
+        
+        if (data.addressType === 'R') {
+          if (data.bname !== '') {
+            extraAddress += data.bname;
+          }
+          if (data.buildingName !== '') {
+            extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+          }
+          fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+        }
+     
+        console.log(fullAddress);  // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+      }
+     
 
+    return (
+        <DaumPostcode
+            onComplete={handleAddress}
+      />
     )
 }
 
