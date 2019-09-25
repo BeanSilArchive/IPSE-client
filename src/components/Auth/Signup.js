745b894faffe676c564a0c3bc2ad1491c171677f
Dotsung <dotsung22@gmail.com>
2019-09-25 10:54:01 +0900

    Add Signup Validation

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -207,45 +207,47 @@ const Form = styled.form`
       }
     }
   }
-
-  #btn {
-    display: flex;
-    width: 100%;
-    height: 50%;
-    flex-direction: row;
-    padding-right: 20%;
-    box-sizing: border-box;
-
-    #Login {
-      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-      border: none;
-      width: 45%;
-      height: 50%;
-      border-radius: 5px;
-      margin-right: 20px;
-      color: white;
-      letter-spacing: 0.5px;
-      background-color: ${oc.gray[9]};
-      font-size: 1.3rem;
-      cursor: pointer;
-      outline: none;
-    }
-    #Register {
-      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-      border: none;
-      width: 45%;
-      height: 50%;
-      border-radius: 5px;
-      letter-spacing: 0.5px;
-      background-color: rgb(255, 255, 255);
-      font-size: 1.3rem;
-      cursor: pointer;
-      outline: none;
-    }
-  }
 `;
 
-const SchoolButton = styled.button`
+const ButtonWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100px;
+  flex-direction: row;
+  padding-right: 20%;
+  box-sizing: border-box;
+
+  #Login {
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+    border: none;
+    width: 45%;
+    height: 100%;
+    border-radius: 5px;
+    margin-right: 20px;
+    color: white;
+    letter-spacing: 0.5px;
+    background-color: ${oc.gray[9]};
+    font-size: 1.3rem;
+    cursor: pointer;
+    outline: none;
+  }
+  #Register {
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+    border: none;
+    width: 45%;
+    height: 100%;
+    border-radius: 5px;
+    letter-spacing: 0.5px;
+    background-color: rgb(255, 255, 255);
+    font-size: 1.3rem;
+    cursor: pointer;
+    outline: none;
+  }
+
+  ${props => (props.spinner ? `display:none` : ``)};
+`;
+
+const SchoolButton = styled.div`
   border: none;
   width: 120px;
   height: 40px;
@@ -256,6 +258,9 @@ const SchoolButton = styled.button`
   color: white;
   background-color: ${oc.gray[8]};
   font-size: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Spacer = styled.div`
@@ -266,6 +271,76 @@ const ErrMsg = styled.p`
   color: ${oc.red[7]};
   margin: 1rem 0;
   font-size: 1rem;
+`;
+
+const Spinner = styled.div`
+  ${props => (props.spinner ? `display:block` : `display:none`)};
+
+  width: 100%;
+  padding-right: 20%;
+  box-sizing: border-box;
+  .spinner {
+    margin: auto;
+    width: 100%;
+    height: 60px;
+    text-align: center;
+    font-size: 30px;
+  }
+
+  .spinner > div {
+    background-color: #333;
+    height: 100%;
+    width: 10px;
+    margin-right: 2px;
+    display: inline-block;
+
+    -webkit-animation: sk-stretchdelay 1.2s infinite ease-in-out;
+    animation: sk-stretchdelay 1.2s infinite ease-in-out;
+  }
+
+  .spinner .rect2 {
+    -webkit-animation-delay: -1.1s;
+    animation-delay: -1.1s;
+  }
+
+  .spinner .rect3 {
+    -webkit-animation-delay: -1s;
+    animation-delay: -1s;
+  }
+
+  .spinner .rect4 {
+    -webkit-animation-delay: -0.9s;
+    animation-delay: -0.9s;
+  }
+
+  .spinner .rect5 {
+    -webkit-animation-delay: -0.8s;
+    animation-delay: -0.8s;
+  }
+
+  @-webkit-keyframes sk-stretchdelay {
+    0%,
+    40%,
+    100% {
+      -webkit-transform: scaleY(0.4);
+    }
+    20% {
+      -webkit-transform: scaleY(1);
+    }
+  }
+
+  @keyframes sk-stretchdelay {
+    0%,
+    40%,
+    100% {
+      transform: scaleY(0.4);
+      -webkit-transform: scaleY(0.4);
+    }
+    20% {
+      transform: scaleY(1);
+      -webkit-transform: scaleY(1);
+    }
+  }
 `;
 
 function reducer(state, action) {
@@ -289,6 +364,7 @@ const Signin = ({ setIsSignUp }) => {
   const [schoolSeq, setSeq] = useState("");
 
   const [errMsg, setErrMsg] = useState("");
+  const [spinner, setSpinner] = useState(false);
 
   const onChange = e => {
     dispatch(e.target);
@@ -318,14 +394,23 @@ const Signin = ({ setIsSignUp }) => {
       return;
     }
 
+    setSpinner(true);
+
     authApi
       .register({ id, password, name, schoolName, schoolSeq, email })
       .then(result => {
-        console.log(result);
         setIsSignUp(false);
+        setSpinner(false);
       })
       .catch(result => {
-        console.log(result);
+        console.log(result.response.data.error);
+        if (result.response.data.error === "002") {
+          setErrMsg("이미 존재하는 아이디입니다.");
+        }
+        if (result.response.data.error === "001") {
+          setErrMsg("잘못된 입력값입니다.");
+        }
+        setSpinner(false);
       });
   };
 
@@ -403,7 +488,16 @@ const Signin = ({ setIsSignUp }) => {
               />
             </div>
             <ErrMsg>{errMsg}</ErrMsg>
-            <div id="btn">
+            <Spinner spinner={spinner}>
+              <div className="spinner">
+                <div className="rect1"></div>
+                <div className="rect2"></div>
+                <div className="rect3"></div>
+                <div className="rect4"></div>
+                <div className="rect5"></div>
+              </div>
+            </Spinner>
+            <ButtonWrapper spinner={spinner}>
               <button id="Login" type="submit">
                 회원가입
               </button>
@@ -415,7 +509,7 @@ const Signin = ({ setIsSignUp }) => {
               >
                 취소
               </button>
-            </div>
+            </ButtonWrapper>
           </Form>
         </Right>
       </Wrapper>
